f11793b0bedfc17994bf22a55147a12fdc77f3bc
sasaplus1 <sasaplus1@gmail.com>
2020-11-09 16:44:56 +0900

    add index.test.ts

---
+++ index.test.ts
@@ -0,0 +1,21 @@
+import assert = require('assert');
+
+import { transform } from './index';
+
+describe('index', function () {
+  describe('transform', function () {
+    it('can transform code', function () {
+      const code = transform(['<#include "/foo/bar/baz" />'].join(''), {
+        transformerPath: require.resolve(
+          __dirname + '/transforms/add-prefix-to-include.js'
+        ),
+        transformerOptions: {
+          from: '/foo/bar/baz',
+          to: '/path/to/file'
+        }
+      });
+
+      assert(code === '<#include "/path/to/file"/>');
+    });
+  });
+});
